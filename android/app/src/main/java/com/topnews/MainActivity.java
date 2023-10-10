b4706f98bffaf97b6343e05264e2359c5a71e734
ramees_exampur <ramees@exampur.xyz>
2023-10-10 17:49:59 +0530

    splash screen is done

--- android/app/src/main/java/com/topnews/MainActivity.java
+++ android/app/src/main/java/com/topnews/MainActivity.java
@@ -4,9 +4,14 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
-
+import com.zoontek.rnbootsplash.RNBootSplash;
+import android.os.Bundle;
 public class MainActivity extends ReactActivity {
-
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    RNBootSplash.init(this, R.style.BootTheme); // ⬅️ initialize the splash screen
+    super.onCreate(savedInstanceState); // or super.onCreate(null) with react-native-screens
+  }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
--- src/screens/main_screen/main_screen.tsx
+++ src/screens/main_screen/main_screen.tsx
@@ -4,11 +4,15 @@ import {useDispatch} from 'react-redux';
 import {getListOfNewsFromApi} from '../../store/entities/news';
 import {AppDispatch} from '../../store/configure';
 import {View} from 'react-native';
-
+import BootSplash from "react-native-bootsplash";
+type hide = (config?: { fade?: boolean }) => Promise<void>;
 const MainScreen = () => {
   const dispatch = useDispatch<AppDispatch>();
   useEffect(() => {
     dispatch(getListOfNewsFromApi({invalidateCache: false})); //should be handled from middleware
+    setTimeout(() => {
+      BootSplash.hide({ fade: true });
+    }, 1000);
   }, []);
   return (
     <View style={{height: '100%', width: '100%'}}>
